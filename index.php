25fda2a1244892b3f65ed69bfc8c4be418d5bb07
Adekomi AbdulBaaqi <adekomiabdulbaqi3@gmail.com>
2021-02-22 10:12:51 +0100

    test link

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
-<html>
-    <head>
-        <title>Hello World</title>
+<?php
+    if( $_POST["location"] )
+    {
+    $location = $_POST["location"];
+    header( "Location:$location" );
+    exit();
+    }
+    ?>
+    <html>
     <body>
-        <?php echo "Hello, World!!! \n";
-            print "<<< FIFA
-            I am a caxy
-            fella who lovves
-            playing games
-            FIFA"
-            
-        
-        
-        ?>
-
+    <p>Choose a site to visit :</p>
+    <form action="<?php $_PHP_SELF ?>" method="POST">
+    <select name="location">
+    <option value="http://w3c.org">
+    World Wise Web Consortium
+    </option>
+    <option value="http://www.google.com">
+    Google Search Page
+    </option>
+    </select>
+    <input type="submit" />
+    </form>
     </body>
-</html>
+    </html>
